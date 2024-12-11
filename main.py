36dea47129c0e4389a17e61d8704391e34ac26a7
pythonai250824 <pythonai250824@gmail.com>
2024-12-11 21:15:38 +0200

    upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+for i in range(1, 46 + 1):
+    # if i % 5 == 0 and i % 3 == 0:
+    #     print("FizzBuzz")
+    # elif i % 5 == 0:
+    #    print("Fizz")
+    # elif i % 3 == 0:
+    #    print("Buzz")
+    print(i, end="-")
+    if i % 5 == 0:
+       print("Fizz", end="-")
+    if i % 3 == 0:
+       print("Buzz", end="-")
+
